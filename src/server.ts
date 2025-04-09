7d18d9212aa1cc5862e6fd203040187f0ab82684
URSA0112 <Nimone99@gmail.com>
2025-04-09 12:29:44 +0800

    cors

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,18 @@ import connectDB from './config/database';
 import { foodRouter } from './routes/foodRoutes';
 import { categoryRouter } from './routes/categoryRoutes';
 import { orderRouter } from './routes/orderRoutes';
+import cors from 'cors'; 
+
+
 
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-
+app.use(cors());
 app.use('/api/v1/food', foodRouter);
 app.use('/api/v1/category', categoryRouter)
 app.use('api/v1/order', orderRouter)
@@ -24,9 +28,8 @@ connectDB()
   })
   .catch((error) => {
     console.error('MongoDB connection failed:', error);
-    process.exit(1); 
+    process.exit(1);
   });
-  
 
 
 
