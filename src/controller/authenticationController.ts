7a521f58f71c9c4dfed8bf15f0ffcd3aba8b6f49
URSA0112 <Nimone99@gmail.com>
2025-04-17 12:08:36 +0800

    :(

--- src/controller/authenticationController.ts
+++ src/controller/authenticationController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import User from '../schema/userSchema';
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv';
